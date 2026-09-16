05427cacdc2dbac87c8a93b4b7e47e1813e5d39e
Surya-pixel389 <surya138pratap@gmail.com>
2026-09-16 12:58:30 +0530

    Dependencies added, UserRepository created

--- backend/src/main/java/com/aribnb/backend/repository/UserRespository.java
+++ backend/src/main/java/com/aribnb/backend/repository/UserRespository.java
@@ -1,4 +1,13 @@
 package com.aribnb.backend.repository;
 
-public class UserRespository {
+import com.aribnb.backend.entity.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRespository extends JpaRepository<user, Long> {
+    Optional<user> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
