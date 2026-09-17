cd73e8f3058aed21dcfdee53798a7b806ed4340c
Surya-pixel389 <surya138pratap@gmail.com>
2026-09-17 11:25:29 +0530

    Initialized the configuration

--- backend/src/main/java/com/aribnb/backend/config/JwtUtil.java
+++ backend/src/main/java/com/aribnb/backend/config/JwtUtil.java
@@ -1,4 +1,56 @@
 package com.aribnb.backend.config;
 
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+    @Value("${jwt.secret:AIRBNBBACKENDSYSTEMPROJECT}")
+    private String secretKey;
+
+    @Value("${jwt.expiration:86400000}")
+    private Long expiration;
+
+    public String generateToken(String email, Long userId){
+        SecretKey Key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        return Jwts.builder()
+                .subject(email)
+                .claim("userId", userId)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(Key)
+                .compact();
+    }
+
+    public String extractEmail(String token){
+        SecretKey Key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        return Jwts.parser()
+                .verifyWith(Key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token ){
+        try{
+            SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
 }
